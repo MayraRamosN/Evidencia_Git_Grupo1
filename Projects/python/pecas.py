261734dbca511670ee045a938c4101fa08f2d6bf
mayra <alejandraramosnarvaez@gmail.com>
2020-11-08 16:41:28 -0500

    Cambios realizados

---
+++ Projects/python/pecas.py
@@ -0,0 +1,9 @@
+print(" Challenge 2:")
+
+answer=input("What is my favorite food?")
+if answer=="pizza":
+  print("Yep, So Amazing!!")
+else:
+  print("Yuck, Thats no´t it")
+print("Thanks for playing")
+
